2f4b0be0ade0375d571fbf68429de3c560868c5f
Adam Ryman <adamryman@gmail.com>
2017-07-30 14:53:41 -0700

    Update schema and build

--- docker/mysql/build.sh
+++ docker/mysql/build.sh
@@ -5,7 +5,8 @@ set -e
 
 epoc="$(date -u "+%s")"
 tag="myambition/ambition-mysql:$epoc"
+latest="myambition/ambition-mysql:latest"
 echo $tag
 
-docker build -t "$tag"  .
-[[ -n $NO_PUSH ]] || docker push "$tag"
+docker build -t "$tag" -t $latest .
+[[ -n $NO_PUSH ]] || docker push "$tag" && docker push $latest
--- docker/mysql/schemas/000_users.sql
+++ docker/mysql/schemas/000_users.sql
@@ -1,2 +1,8 @@
 CREATE DATABASE IF NOT EXISTS users;
-CREATE TABLE IF NOT EXISTS users.users(id SERIAL PRIMARY KEY, username varchar(255), email varchar(255), trello_id varchar(255), trello_webhookurl varchar(255));
+CREATE TABLE IF NOT EXISTS users.users(
+	id SERIAL PRIMARY KEY,
+	username varchar(255) NOT NULL,
+	email varchar(255) NOT NULL DEFAULT '',
+	trello_id varchar(255) NOT NULL,
+	trello_webhookurl varchar(255) NOT NULL DEFAULT ''
+);
