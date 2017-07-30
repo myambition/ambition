90e8fa179b1aa887304f3282bcee6e857c0597bc
Adam Ryman <adamryman@gmail.com>
2017-07-30 10:21:05 -0700

    Add rello handlers

--- rello/rello-service/handlers/handlers.go
+++ rello/rello-service/handlers/handlers.go
@@ -1,21 +1,129 @@
 package handlers
 
 import (
+	"fmt"
 	"golang.org/x/net/context"
+	"os"
+	"time"
 
+	"github.com/pkg/errors"
+	"google.golang.org/grpc"
+
+	modelSVC "github.com/adamryman/ambition/model/model-service"
 	pb "github.com/adamryman/ambition/rello/rello-service"
+	usersSVC "github.com/adamryman/ambition/users/users-service"
+
+	modelClient "github.com/adamryman/ambition/model/model-service/svc/client/grpc"
+	usersClient "github.com/adamryman/ambition/users/users-service/svc/client/grpc"
+
+	"github.com/davecgh/go-spew/spew"
 )
+
+const trelloTime = "2006-01-02T15:04:05.999Z07:00"
 
 // NewService returns a naïve, stateless implementation of Service.
 func NewService() pb.RelloServer {
-	return relloService{}
+	// TODO: Create ambitionSVC and usersSVC clients
+	// TODO switch to grpc clients
+	modelConnStr := os.Getenv("MODEL_HOST") + ":" + os.Getenv("MODEL_PORT")
+	fmt.Println(modelConnStr)
+	modelConn, err := grpc.Dial(modelConnStr, grpc.WithInsecure(), grpc.WithTimeout(time.Second))
+	m, err := modelClient.New(modelConn)
+	if err != nil {
+		panic(err)
+	}
+	usersConnStr := os.Getenv("USERS_HOST") + ":" + os.Getenv("USERS_PORT")
+	fmt.Println(usersConnStr)
+	usersConn, err := grpc.Dial(usersConnStr, grpc.WithInsecure(), grpc.WithTimeout(time.Second))
+	u, err := usersClient.New(usersConn)
+	if err != nil {
+		panic(err)
+	}
+
+	return relloService{
+		model: m,
+		users: u,
+	}
 }
 
-type relloService struct{}
+type relloService struct {
+	users usersSVC.UsersServer
+	model modelSVC.ModelServer
+}
 
 // CheckListWebhook implements Service.
+// TODO: Auth middleware using user trello webhookcallback
 func (s relloService) CheckListWebhook(ctx context.Context, in *pb.ChecklistUpdate) (*pb.Empty, error) {
-	var resp pb.Empty
-	resp = pb.Empty{}
-	return &resp, nil
+	fmt.Println(spew.Sdump(in))
+	fmt.Println(spew.Sdump(in.GetAction()))
+	fmt.Println(spew.Sdump(in.GetAction().GetMemberCreator))
+
+	user, err := s.users.ReadUser(ctx,
+		&usersSVC.User{
+			Trello: &usersSVC.TrelloInfo{
+				ID: in.GetAction().GetIdMemberCreator(),
+			},
+			Info: &usersSVC.UserInfo{},
+		})
+	if err != nil {
+		//TODO: Wrap error
+		return nil, err
+	}
+
+	cItem := in.Action.Data.CheckItem
+	switch in.Action.Type {
+	case "createCheckItem":
+		// TODO: Map CheckItem.Id to Action.Id
+		// TODO: Map u.Action.MemberCreator.Id to Action.UserId
+		// UserId hardcoded to 1
+		_ = in.Action.MemberCreator.Id
+
+		action, err := s.model.CreateAction(ctx,
+			&modelSVC.Action{
+				Name:   cItem.GetName(),
+				UserID: user.GetID(),
+			})
+		if err != nil {
+			fmt.Println(errors.Wrap(err, "cannot create action"))
+			break
+		}
+		fmt.Printf("%s action created\n", action.Name)
+	case "updateCheckItemStateOnCard":
+		if cItem.State == "incomplete" {
+			fmt.Printf("%q is being unchecked\n", cItem.Name)
+			break
+		}
+		action, err := s.model.ReadAction(ctx,
+			&modelSVC.Action{
+				Name:   cItem.GetName(),
+				UserID: user.GetID(),
+			})
+		dateString := &in.Action.Date
+		date, err := time.Parse(trelloTime, *dateString)
+		_ = date
+		if err != nil {
+			fmt.Println(errors.Wrap(err, "time parsing failed"))
+			break
+		}
+		// TODO: log occurrence
+		_, err = s.model.CreateOccurrence(ctx,
+			&modelSVC.CreateOccurrenceRequest{
+				UserID: user.GetID(),
+				Occurrence: &modelSVC.Occurrence{
+					ActionID: action.GetID(),
+					// TODO: make sure this time format is fine
+					Datetime: date.String(),
+				},
+			})
+		if err != nil {
+			fmt.Println(errors.Wrap(err, "error creating occurrence"))
+			break
+		}
+
+		// TODO: Create Occurrence with the ActionId from CheckItem.Id
+		// Maps to action id
+	default:
+		fmt.Println(in.Action.Type)
+	}
+	return nil, nil
 }
