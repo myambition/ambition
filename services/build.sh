be076bf281583c40d9e1a3c3d4d1dd9277fa37d1
Adam Ryman <adamryman@gmail.com>
2017-07-30 14:54:01 -0700

    Update build script to run from anywhere

--- services/build.sh
+++ services/build.sh
@@ -3,12 +3,14 @@
 set -e
 [[ -z $DEBUG ]] || set -x
 
+cur_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 services="rello users model"
 
 for service in $services; do
 	(
 		echo "building $service"
-		cd $service;
+		cd $cur_dir/$service;
 		mkdir -p target
 		go build -o ./target/newrun ./${service}-service/cmd/${service}-server/
 		mv ./target/newrun ./target/run
