03296b10813e94daf75aae92adcd1f52ee1bc42c
Adam Ryman <adamryman@gmail.com>
2017-07-30 12:04:24 -0700

    Move services to services directory (#2)

---
+++ services/model/internal/mysql/db.go
@@ -0,0 +1,87 @@
+package mysql
+
+import (
+	"database/sql"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/pkg/errors"
+	//"github.com/adamryman/db"
+
+	pb "github.com/adamryman/ambition/services/model/model-service"
+)
+
+func Open(conn string) (*Database, error) {
+	d, err := sql.Open("mysql", conn)
+	if err != nil {
+		return nil, errors.Wrapf(err, "cannot connect to mysql with %s", conn)
+	}
+	//if err := d.Ping(); err != nil {
+	//return nil, errors.Wrapf(err, "cannot make initial database connection to %s", conn)
+	//}
+
+	return &Database{d}, nil
+}
+
+type Database struct {
+	db *sql.DB
+}
+
+func (d *Database) CreateAction(in *pb.Action) (*pb.Action, error) {
+	const query = `INSERT INTO actions(action_name, user_id) VALUES (?, ?)`
+	id, err := exec(d.db, query, in.GetName(), in.GetUserID())
+	if err != nil {
+		return nil, err
+	}
+	in.ID = id
+
+	return in, nil
+}
+
+func (d *Database) CreateOccurrence(in *pb.Occurrence) (*pb.Occurrence, error) {
+	const query = `INSERT INTO occurrences(action_id, datetime, data) VALUES (?, ?, ?)`
+	id, err := exec(d.db, query, in.GetActionID(), in.GetDatetime(), in.GetData())
+	if err != nil {
+		return nil, err
+	}
+	in.ID = id
+
+	return in, nil
+}
+
+func (d *Database) ReadActionByID(id int64) (*pb.Action, error) {
+	const query = `SELECT * FROM actions WHERE id=?`
+	resp := d.db.QueryRow(query, id)
+	var action pb.Action
+	err := resp.Scan(&action.ID, &action.Name, &action.UserID)
+	if err != nil {
+		return nil, err
+	}
+
+	return &action, nil
+}
+
+func (d *Database) ReadActionByNameAndUserID(name string, userID int64) (*pb.Action, error) {
+	const query = `SELECT * FROM actions WHERE action_name=? AND user_id=?`
+	resp := d.db.QueryRow(query, name, userID)
+	var action pb.Action
+	err := resp.Scan(&action.ID, &action.Name, &action.UserID)
+	if err != nil {
+		return nil, err
+	}
+
+	return &action, nil
+}
+
+// exec calls db.db.Exec with passed arguments and returns the id of the LastInsertId
+func exec(db *sql.DB, query string, args ...interface{}) (int64, error) {
+	resp, err := db.Exec(query, args...)
+	if err != nil {
+		return 0, errors.Wrapf(err, "unable to exec query: %v", query)
+	}
+
+	id, err := resp.LastInsertId()
+	if err != nil {
+		return 0, errors.Wrapf(err, "unable to get last id after query: %v", query)
+	}
+
+	return id, nil
+}
---
+++ services/model/model-service/handlers/handlers.go
@@ -0,0 +1,137 @@
+package handlers
+
+import (
+	"fmt"
+	"golang.org/x/net/context"
+	//"os"
+	"time"
+
+	"github.com/pkg/errors"
+
+	pb "github.com/adamryman/ambition/services/model/model-service"
+	//sql "github.com/adamryman/model/internal/sqlite"
+	sql "github.com/adamryman/ambition/services/model/internal/mysql"
+	"github.com/adamryman/kit/dbconn"
+)
+
+// NewService returns a naïve, stateless implementation of Service.
+func NewService() pb.ModelServer {
+	//database, err := sql.Open(os.Getenv("SQLITE3"))
+
+	database, err := sql.Open(dbconn.FromENV("MYSQL").MySQL())
+	if err != nil {
+		// TODO: Do not panic, start something to try connection over and over.
+		// Maybe 100 times?
+		// DEBUG_SVC=1 then do like 3.
+		// There will also need to be retry logic for the database methods
+		panic(err)
+	}
+	return ambitionService{
+		db: database,
+	}
+}
+
+type ambitionService struct {
+	db *sql.Database
+}
+
+// CreateAction implements Service.
+func (s ambitionService) CreateAction(ctx context.Context, in *pb.Action) (*pb.Action, error) {
+	// TODO: Input validation
+	a, err := s.db.CreateAction(in)
+	if err != nil {
+		return nil, errors.Wrap(err, "cannot create action")
+	}
+	return a, nil
+}
+
+// CreateOccurrence implements Service.
+func (s ambitionService) CreateOccurrence(ctx context.Context, in *pb.CreateOccurrenceRequest) (*pb.Occurrence, error) {
+	// TODO: Make sure database accepts this time format
+	utc7, err := time.LoadLocation("America/Los_Angeles")
+	if err != nil {
+		return nil, errors.Wrap(err, "cannot create time location UTC-7")
+	}
+	nowutc := time.Now()
+	fmt.Println(nowutc)
+	now := nowutc.In(utc7).String()
+	fmt.Println(now)
+
+	occurrence := in.GetOccurrence()
+	if occurrence == nil {
+		return nil, errors.New("cannot create nil occurrence")
+	}
+	if occurrence.GetDatetime() == "" {
+		occurrence.Datetime = now
+	}
+
+	action, err := s.db.ReadActionByID(occurrence.GetActionID())
+	if err != nil {
+		return nil, errors.Wrap(err, "cannot read action")
+	}
+	if action.GetUserID() != in.GetUserID() {
+		// TODO: Replace this "logging" with real logging
+		fmt.Printf("action-user-id: %d\nuser-id:%d\n", action.GetUserID(), in.GetUserID())
+		return nil, errors.New("cannot create occurrence for action not owned by user")
+	}
+
+	o, err := s.db.CreateOccurrence(occurrence)
+	if err != nil {
+		return nil, errors.Wrap(err, "cannot create occurrence")
+	}
+	return o, nil
+}
+
+// ReadAction implements Service.
+func (s ambitionService) ReadAction(ctx context.Context, in *pb.Action) (*pb.Action, error) {
+	if in.GetID() != 0 {
+		a, err := s.db.ReadActionByID(in.GetID())
+		if err != nil {
+
+			return nil, errors.Wrap(err, "cannot read action")
+		}
+		return a, nil
+	}
+	if name, userID := in.GetName(), in.GetUserID(); name != "" && userID != 0 {
+		a, err := s.db.ReadActionByNameAndUserID(name, userID)
+		if err != nil {
+			return nil, errors.Wrap(err, "cannot read action")
+		}
+		return a, nil
+	}
+	return nil, errors.New("cannot read action, need ID or BOTH UserID and Name")
+}
+
+// ReadActions implements Service.
+// TODO: Implement
+func (s ambitionService) ReadActions(ctx context.Context, in *pb.User) (*pb.ActionsResponse, error) {
+	// TODO: Input validation
+	var resp pb.ActionsResponse
+	resp = pb.ActionsResponse{
+	// Actions:
+	}
+	return &resp, nil
+}
+
+// ReadOccurrences implements Service.
+// TODO: Implement
+func (s ambitionService) ReadOccurrences(ctx context.Context, in *pb.Action) (*pb.OccurrencesResponse, error) {
+	// TODO: Input validation
+	var resp pb.OccurrencesResponse
+	resp = pb.OccurrencesResponse{
+	// Occurrences:
+	}
+	return &resp, nil
+}
+
+// ReadOccurrencesByDate implements Service.
+func (s ambitionService) ReadOccurrencesByDate(ctx context.Context, in *pb.OccurrencesByDateReq) (*pb.OccurrencesResponse, error) {
+	var resp pb.OccurrencesResponse
+	if in.GetActionID() != 0 {
+
+	}
+	resp = pb.OccurrencesResponse{
+	// Occurrences:
+	}
+	return &resp, nil
+}
