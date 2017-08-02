8312c42acdac26bd189bcc208b805fe1259d57d5
Adam Ryman <adamryman@gmail.com>
2017-08-02 07:02:37 -0700

    Update travis to use latest docker

--- docker_build.sh
+++ docker_build.sh
@@ -9,12 +9,19 @@ cur_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 services="rello users model"
 
+if [[ "$CI" == "true" ]]; then
+	curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -;
+	sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable";
+	sudo apt-get update;
+	sudo apt-get -y install docker-ce;
+fi
+
 for service in $services; do
 	(
 		echo "building $service"
 		tag="myambition/ambition-${service}:latest"
 		echo $tag
 		docker build -t $tag --build-arg service=${service} .
-		docker push $tag &
+		docker push $tag
 	)
 done
